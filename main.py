81566f4512c7576c2dd7b487fb7561086ba814b4
Vesth <kevanchuach@yahoo.com>
2016-06-13 21:08:02 +0800

    Cover Page
    Cover Page files, bootstrap 4 and an example of the next page. See index
    for the dashboard example.

--- main.py
+++ main.py
@@ -30,6 +30,28 @@ class MainPage(webapp2.RequestHandler):
 		}
 		self.response.out.write(template.render(context))
 
+class index(webapp2.RequestHandler): 
+	def get(self):
+		current_time = datetime.datetime.now()
+		user = users.get_current_user()
+		login_url = users.create_login_url(self.request.path)
+		logout_url = users.create_logout_url(self.request.path)
 
-application = webapp2.WSGIApplication([('/', MainPage)],
+		userprefs = models.get_userprefs()
+		if userprefs:
+			current_time += datetime.timedelta(0, 0, 0, 0, 0, userprefs.tz_offset)
+
+		template = template_env.get_template('index.html')
+		context = {
+			'current_time': current_time,
+			'user': user,
+			'login_url': login_url,
+			'logout_url': logout_url,
+			'userprefs': userprefs,
+		}
+		self.response.out.write(template.render(context))
+
+
+application = webapp2.WSGIApplication([('/', MainPage),
+									   ('/index', index)],
 									  debug=True)
