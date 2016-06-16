263767c8d4534688d8368d06b896163f4bc5e073
adjacentelbow <1400x900@gmail.com>
2016-06-16 15:37:25 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -31,10 +31,25 @@ class MainPage(webapp2.RequestHandler):
 		self.response.out.write(template.render(context))
 
 class Index(webapp2.RequestHandler):
-    """Incomplete"""
-    def get(self):
-        template = template_env.get_template('index.html')
-        self.response.out.write(template.render())
+	def get(self):
+		current_time = datetime.datetime.now()
+		user = users.get_current_user()
+		login_url = users.create_login_url(self.request.path)
+		logout_url = users.create_logout_url(self.request.path)
+
+		userprefs = models.get_userprefs()
+		if userprefs:
+			current_time += datetime.timedelta(0, 0, 0, 0, 0, userprefs.tz_offset)
+
+		template = template_env.get_template('index.html')
+		context = {
+			'current_time': current_time,
+			'user': user,
+			'login_url': login_url,
+			'logout_url': logout_url,
+			'userprefs': userprefs,
+		}
+		self.response.out.write(template.render(context))
 		
 class About(webapp2.RequestHandler):
     """Incomplete"""
